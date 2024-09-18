aa94617f8f7e7fd26d921b6f16857cff925923d1
Lucas Galhardo Lima <lima.galhardo@gmail.com>
2024-09-17 22:41:05 -0300

    feat: removed unused files, created header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "~/assets/images/logo.svg";
+
+export const Header: React.FC = () => {
+  return (
+    <header className="bg-[#002A42] w-full h-[50px] flex items-center px-6">
+      <img src={logo} height={26} alt="logo" />
+    </header>
+  );
+};
